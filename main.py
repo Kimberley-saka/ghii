d23f7eb9c11d16004acd0deb7af0e2d1dec12a53
Kimberley saka <kimberleysaka@gmail.com>
2023-10-30 19:52:47 +0200

    testing file

---
+++ main.py
@@ -0,0 +1,33 @@
+from graph import WeightedGraph
+
+
+graph = WeightedGraph()
+
+graph.add_vertex('Mchinji')
+graph.add_vertex('Kasungu')
+graph.add_vertex('Lilongwe')
+graph.add_vertex('Dowa')
+graph.add_vertex('Ntchisi')
+graph.add_vertex('Nkhotakota')
+graph.add_vertex('Salima')
+graph.add_vertex('Dedza')
+graph.add_vertex('Ntcheu')
+
+graph.add_edge('Mchinji', 'Kasungu', 141)
+graph.add_edge('Mchinji', 'Lilongwe', 109)
+
+print(graph.adjancent_list)
+
+"""
+    test_graph = {
+        {'Mchinji': {'kasungu': 141, 'Lilongwe': 109}},
+        {'kasungu': {'Mchinji': 141, 'Dowa': 117, 'Ntchisi': 66}},
+        {'Lilongwe': {'Dowa': 55, 'Mchinji':109, 'Dedza': 92}},
+        {'Dowa': {'Kasungu': 117, 'Ntchisi': 38, 'Lilongwe':55, 'Salima': 67}},
+        {'Ntchisi': {'Nkhotakota': 66, 'Dowa': 38, 'Kasungu':66}},
+        {'Nkhotakota': {'Salima': 112, 'Ntchisi': 66}},
+        {'Salima': {'Dedza': 96, 'Dowa': 67, 'Nkhotakota': 112}}, 
+        {'Dedza': {'Ntcheu': 74, 'Salima': 96, 'Lilongwe': 92}},
+        {'Ntcheu': {'Dedza': 74}}
+    }
+"""

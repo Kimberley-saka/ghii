7d8dde546b5f9d1d8554df1fb635ea38e6aeab5b
Kimberley saka <kimberleysaka@gmail.com>
2023-10-31 07:16:40 +0200

    added the shortest path function

--- graph.py
+++ graph.py
@@ -2,6 +2,8 @@
 Weighted graph
 """
 from typing import List, Dict
+import sys
+from heapq import heapify, heappush
 
 
 class WeightedGraph:
@@ -15,7 +17,7 @@ class WeightedGraph:
 
     def add_vertex(self, vertex: str) -> None:
         """
-        add vertices
+        adds vertices
         """
         if vertex not in self.vertex_list:
             self.vertex_list.append(vertex)
@@ -25,7 +27,7 @@ class WeightedGraph:
 
     def add_edge(self, vertex1: str, vertex2: str, weight: int) -> None:
         """
-        Add edge to adjacent list given the graphs vertices and weight
+        Adds edges to adjacent list given the graphs vertices and weight
         """
         temp_list = []
         if vertex1 in self.vertex_list and vertex2 in self.vertex_list:
@@ -42,40 +44,45 @@ class WeightedGraph:
             print('vertex doesnt exist')
 
 
-def shortest_path(graph: Dict[str, List[str, int]]) -> List:
+def shortest_path(graph: Dict[str, List], src: str, dest: str) -> None:
     """
-    Find the shortest path from one district to another
-    in given weighted graph
+    Finds the shortest path from one vertex(district) to another
+    in a weighted graph
     """
-
-
-graph = WeightedGraph()
-
-graph.add_vertex('Mchinji')
-graph.add_vertex('Kasungu')
-graph.add_vertex('Lilongwe')
-graph.add_vertex('Dowa')
-graph.add_vertex('Ntchisi')
-graph.add_vertex('Nkhotakota')
-graph.add_vertex('Salima')
-graph.add_vertex('Dedza')
-graph.add_vertex('Ntcheu')
-
-graph.add_edge('Mchinji', 'Kasungu', 141)
-graph.add_edge('Mchinji', 'Lilongwe', 109)
-
-print(graph.adjancent_list)
-
-"""
-    test_graph = {
-        {'Mchinji': {'kasungu': 141, 'Lilongwe': 109}},
-        {'kasungu': {'Mchinji': 141, 'Dowa': 117, 'Ntchisi': 66}},
-        {'Lilongwe': {'Dowa': 55, 'Mchinji':109, 'Dedza': 92}},
-        {'Dowa': {'Kasungu': 117, 'Ntchisi': 38, 'Lilongwe':55, 'Salima': 67}},
-        {'Ntchisi': {'Nkhotakota': 66, 'Dowa': 38, 'Kasungu':66}},
-        {'Nkhotakota': {'Salima': 112, 'Ntchisi': 66}},
-        {'Salima': {'Dedza': 96, 'Dowa': 67, 'Nkhotakota': 112}}, 
-        {'Dedza': {'Ntcheu': 74, 'Salima': 96, 'Lilongwe': 92}},
-        {'Ntcheu': {'Dedza': 74}}
+    infinity = sys.maxsize
+    vertex_data ={
+        'Mchinji': {'cost': infinity, 'pred':[]},
+        'Kasungu': {'cost': infinity, 'pred':[]},
+        'Lilongwe': {'cost': infinity, 'pred':[]},
+        'Dowa': {'cost': infinity, 'pred':[]},
+        'Ntchisi': {'cost': infinity, 'pred':[]},
+        'Nkhotakota': {'cost': infinity, 'pred':[]},
+        'Salima': {'cost': infinity, 'pred':[]},
+        'Dedza': {'cost': infinity, 'pred':[]},
+        'Ntcheu': {'cost': infinity, 'pred':[]},
     }
-"""
+
+    visited = []
+    vertex_data[src]['cost'] = 0
+    temp = src
+
+    for _ in range(8):
+        if temp not in visited:
+            visited.append(temp)
+
+            min_heap = []
+            for j in graph[temp]:
+                if j not in visited:
+                    cost = vertex_data[temp]['cost'] + graph[temp][j] 
+                    if cost < vertex_data[j]['cost']:
+                        vertex_data[j]['cost'] = cost
+                        vertex_data[j]['pred'] = vertex_data[temp]['pred'] + [temp]
+                    
+                    heappush(min_heap, (vertex_data[j]['cost'], j))
+        
+        if min_heap:
+            heapify(min_heap)
+            temp = min_heap[0][1]
+
+    print('Total distance of path:' + str(vertex_data[dest]['cost']))
+    print('Shortest path: ' + str(vertex_data[dest]['pred'] + [dest]))
--- main.py
+++ main.py
@@ -1,33 +1,40 @@
-from graph import WeightedGraph
-
-
-graph = WeightedGraph()
-
-graph.add_vertex('Mchinji')
-graph.add_vertex('Kasungu')
-graph.add_vertex('Lilongwe')
-graph.add_vertex('Dowa')
-graph.add_vertex('Ntchisi')
-graph.add_vertex('Nkhotakota')
-graph.add_vertex('Salima')
-graph.add_vertex('Dedza')
-graph.add_vertex('Ntcheu')
-
-graph.add_edge('Mchinji', 'Kasungu', 141)
-graph.add_edge('Mchinji', 'Lilongwe', 109)
-
-print(graph.adjancent_list)
-
 """
-    test_graph = {
-        {'Mchinji': {'kasungu': 141, 'Lilongwe': 109}},
-        {'kasungu': {'Mchinji': 141, 'Dowa': 117, 'Ntchisi': 66}},
-        {'Lilongwe': {'Dowa': 55, 'Mchinji':109, 'Dedza': 92}},
-        {'Dowa': {'Kasungu': 117, 'Ntchisi': 38, 'Lilongwe':55, 'Salima': 67}},
-        {'Ntchisi': {'Nkhotakota': 66, 'Dowa': 38, 'Kasungu':66}},
-        {'Nkhotakota': {'Salima': 112, 'Ntchisi': 66}},
-        {'Salima': {'Dedza': 96, 'Dowa': 67, 'Nkhotakota': 112}}, 
-        {'Dedza': {'Ntcheu': 74, 'Salima': 96, 'Lilongwe': 92}},
-        {'Ntcheu': {'Dedza': 74}}
+test file
+"""
+from graph import WeightedGraph, shortest_path
+
+
+if __name__ == '__main__':
+    """
+    graph = WeightedGraph()
+
+    graph.add_vertex('Mchinji')
+    graph.add_vertex('Kasungu')
+    graph.add_vertex('Lilongwe')
+    graph.add_vertex('Dowa')
+    graph.add_vertex('Ntchisi')
+    graph.add_vertex('Nkhotakota')
+    graph.add_vertex('Salima')
+    graph.add_vertex('Dedza')
+    graph.add_vertex('Ntcheu')
+
+    graph.add_edge('Mchinji', 'Kasungu', 141)
+    graph.add_edge('Mchinji', 'Lilongwe', 109)
+
+    print(graph.adjancent_list)
+    """
+
+    graph = {
+    'Mchinji': {'Kasungu': 141, 'Lilongwe': 109},
+    'Kasungu': {'Mchinji': 141, 'Dowa': 117, 'Ntchisi': 66},
+    'Lilongwe': {'Dowa': 55, 'Mchinji':109, 'Dedza': 92},
+    'Dowa': {'Kasungu': 117, 'Ntchisi': 38, 'Lilongwe':55, 'Salima': 67},
+    'Ntchisi': {'Nkhotakota': 66, 'Dowa': 38, 'Kasungu':66},
+    'Nkhotakota': {'Salima': 112, 'Ntchisi': 66},
+    'Salima': {'Dedza': 96, 'Dowa': 67, 'Nkhotakota': 112}, 
+    'Dedza': {'Ntcheu': 74, 'Salima': 96, 'Lilongwe': 92},
+    'Ntcheu': {'Dedza': 74}
     }
-"""
+
+
+    print (shortest_path(graph, 'Mchinji', 'Nkhotakota'))
